ebeddc5e203c05e765f543f108c75e2e8125fa1e
ChrisHYKim <ckim96kim@gmail.com>
2024-10-25 00:36:27 +0900

    241024 project

---
+++ 05_vars-inclusions/epel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BASEDIR=/home/ansible/ansible/05_vars-inclusions
+cd $BASEDIR
+
+#1) inventory 파일 생성
+cat << EOF > allhosts
+web1
+web2
+web3
+web4
+EOF
+
+# 2) 플레이북 실행
+ansible-playbook –y allhost epel.yml
